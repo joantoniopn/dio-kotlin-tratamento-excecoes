3273ff2b0670b9c30ebd59feab808024dcd0b972
Joao Neto <joao-antoniopn@live.com>
2023-09-12 23:08:15 -0300

    Finalizado Aula 5

--- main.kt
+++ main.kt
@@ -13,12 +13,29 @@ fun calculaDivisao(a: Int = 0, b: Int = 0) {
 }
 
 //Aula 3
-
 fun calculaDivisaoMelhorada (a: Int, b: Int): Int {
     return try { a/b } catch(e: ArithmeticException) { 0 }
 }
 
+//Aula 5
+class IllegalVoterException(message: String) : Throwable(message)
+
+//Aula 5
+@Throws(IllegalVoterException::class)
+fun registraVoto(name: String, age: Int) {
+    if(age < 16) {
+        throw IllegalVoterException("$name nao pode votar.")
+    }
+    println("Voto de ${name} realizado com sucesso!")
+}
+
 fun main() {
     //calculaDivisao(1,0)
-    println(calculaDivisaoMelhorada(4, 2))
+    //println(calculaDivisaoMelhorada(4, 2))
+
+    var quantidadeVotos : Int = 0
+        quantidadeVotos += try { registraVoto("Joao", 30); 1; } catch (e: IllegalVoterException) { 0 };
+        quantidadeVotos += try { registraVoto("Marcos", 10); 1; } catch (e: IllegalVoterException) { 0 };
+        quantidadeVotos += try { registraVoto("Renan", 50); 1; } catch (e: IllegalVoterException) { 0 };
+    println(quantidadeVotos)
 }
